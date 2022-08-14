9fdb71ad733d1d3d82a3a0ab604f37068bfb1651
Source Academy User <No public email provided>
2022-08-14 12:34:54 +0800

    Added is_biggie_size function

--- Burgerjoint.js
+++ Burgerjoint.js
@@ -1,7 +1,12 @@
 function biggie_size(normal_combo) {
-    return normal_combo + 4
+    return normal_combo + 4;
 }
 
 function unbiggie_size(upsized_combo) {
-    return upsized_combo - 4
+    return upsized_combo - 4;
 }
+
+function is_biggie_size(combo) {
+    return combo > 4;
+}
+
