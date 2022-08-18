a298c079c55fc0de606caee7add2d99b13c17ac7
Source Academy User <No public email provided>
2022-08-18 14:09:23 +0800

    Changes made from Source Academy

--- quests/quest1.js
+++ quests/quest1.js
@@ -1,29 +1,5 @@
 import {heart, quarter_turn_left, from_url, stackn, square, blank, stack_frac, beside_frac, sail, corner, nova, red, stack, flip_horiz, quarter_turn_right, turn_upside_down, make_cross, rcross, beside, show} from "rune";
 
-/*function edge(rune, count) {
-    return beside_frac(1/count, stackn(count, rune), 
-    stack_frac(1/count, stacking(rune, count-1), stackn(count-1,
-    beside_frac((count-2)/(count-1), stack_frac(1/count,  
-    stacking(rune, count-2), blank), rune))));
-}
-
-function stacking(rune, count) {
-    return quarter_turn_left(stackn(count, quarter_turn_right(rune)));
-}*/
-
-function edge_column(rune, count) {
-    return stackn(count, rune);
-}
-
-function edge_row(rune, count) {
-    return quarter_turn_left(stackn(count, quarter_turn_right(rune)));
-}
-
-function middle(rune) {
-    const middle_column1 = stack(quarter_turn_right(rune), rune);
-    const middle_column2 = stack(turn_upside_down(rune), quarter_turn_left(rune));
-    return beside(middle_column1, middle_column2);
-}
 
 function persian(rune, count) {
     const column = stackn(count, rune);
