c8278760b42c101cbdfac102476d17bccf074d11
Source Academy User <No public email provided>
2022-08-18 09:36:03 +0800

    Changes made from Source Academy

---
+++ missions/mission1/q4.js
@@ -0,0 +1,15 @@
+import {heart, sail, corner, nova, red, stack, flip_horiz, quarter_turn_right, turn_upside_down, make_cross, stack_beside, rcross, beside, show} from "rune";
+
+function mosaic(r1, r2, r3, r4) {
+    return stack(
+           beside(r4, r1),
+           beside(r3, r2)
+        );
+}
+
+function transform_mosaic(r1, r2, r3, r4, transform) {
+    return transform(mosaic(r1, r2, r3, r4));
+}
+
+// Test
+show(transform_mosaic(rcross, sail, corner, nova, make_cross));
